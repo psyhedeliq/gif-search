76f531dc0f180827f58933d056c7070c4e44efe1
Cornel <filipcornelcristian@gmail.com>
2020-10-23 02:08:43 +0300

    added search box

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,18 @@
 <script>
+  let search = '';
+
+  function formSubmitted(event) {
+    console.log(search);
+  }
 </script>
 
 <style>
 </style>
+
+<form on:submit|preventDefault={formSubmitted}>
+  <label for="search">Search</label>
+  <input bind:value={search} id="search" name="search" />
+  <button type="submit">Submit</button>
+</form>
+
+<h2>{search}</h2>
