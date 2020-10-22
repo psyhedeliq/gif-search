bc7275e05558ec4276a846903658fdd134f47528
Cornel <filipcornelcristian@gmail.com>
2020-10-23 02:22:43 +0300

    added api url

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,18 @@
 <script>
   let search = '';
+  let loading = false;
+  let API_URL =
+    'https://api.giphy.com/v1/gifs/search?api_key=JnxTmEGKXjZeUKBzRjTQoMDg8OX8pS5U&rating=pg&q=';
 
-  function formSubmitted(event) {
-    console.log(search);
+  let gifs = [];
+
+  async function formSubmitted(event) {
+    loading = true;
+    const url = `${API_URL}${search}`;
+    const response = await fetch(url);
+    const json = await response.json();
+    gifs = json.data.map((gif) => gif.images.fixed_height.url);
+    console.log(gifs);
   }
 </script>
 
@@ -15,4 +25,8 @@
   <button type="submit">Submit</button>
 </form>
 
-<h2>{search}</h2>
+{#if loading}
+  <img
+    src="https://media.giphy.com/media/3y0oCOkdKKRi0/giphy.gif"
+    alt="Elmer Runnig Circles" />
+{/if}
